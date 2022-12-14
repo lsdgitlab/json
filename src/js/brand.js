c7daef940b42a469e1b07297539b4afb6f323c29
Ravindra Dhadave <110818058+lsdgitlab@users.noreply.github.com>
2022-12-14 20:39:06 +0530

    Add files via upload

--- src/js/brand.js
+++ src/js/brand.js
@@ -5,7 +5,7 @@ console.log('Brand Data =>', brandData)
 window.addEventListener('DOMContentLoaded', () => {
   topheader(0)
   brandDrpDwn(brandData)
-  
+  uCat(0)
 })
 
 function topheader(ele) {
@@ -23,10 +23,10 @@ function topheader(ele) {
   const flmCnt = document.querySelector('#filmSec .second-title span')
   flmCnt.textContent = '(' + brandData[ele].Film.length + ')'
   const smcCatcnt = document.querySelector('#smcSect .second-title span')
-  smcCatcnt.innerHTML = '(' + brandData[ele].Category.length + ')';
-  const smcat = document.getElementById('cateOne')  
+  smcCatcnt.innerHTML = '(' + brandData[ele].Category.length + ')'
+  const smcat = document.getElementById('cateOne')
   // load more category
-  const smcLoadM = document.querySelector('#catLoadmore button');
+  const smcLoadM = document.querySelector('#catLoadmore button')
 
   let loadMbtn = document.querySelector('.loadMoreSec button')
   loadMbtn.addEventListener('click', () => {
@@ -36,8 +36,8 @@ function topheader(ele) {
   filmItemInx()
   function filmItemInx() {
     let filcard = filmSec.innerHTML
-    var filmIndx = document.querySelectorAll('#filmCont .filmes-card').length;
-    console.log(filmIndx);
+    var filmIndx = document.querySelectorAll('#filmCont .filmes-card').length
+    console.log(filmIndx)
 
     for (var i = 0; i < 3; i++) {
       var item = brandData[ele].Film[filmIndx]
@@ -57,7 +57,9 @@ function topheader(ele) {
             '<img src="./img/king.png" alt="">' +
             '</div>' +
             '<div class="play-btn">' +
-            '<img src="./img/play-btn-small.png" alt="" class="youtube" ytSrc="' +item.filmBig +'">' +
+            '<img src="./img/play-btn-small.png" alt="" class="youtube" ytSrc="' +
+            item.filmBig +
+            '">' +
             '</div>' +
             '</div>' +
             '</div>' +
@@ -99,71 +101,69 @@ function topheader(ele) {
     filmSec.innerHTML = filcard
   }
 
+  smcLoadM.addEventListener('click', () => {
+    smcItemIdx()
+  })
 
-smcLoadM.addEventListener('click', ()=>{
-  smcItemIdx()  
-})
+  smcItemIdx()
+  function smcItemIdx() {
+    let smcCard = smcat.innerHTML
+    var smcIndex = document.querySelectorAll('#smcSect .filmes-card').length
+    for (var n = 0; n < 4; n++) {
+      let item = brandData[ele].Category[smcIndex]
+      // console.log(item);
+      smcIndex++
 
-
-smcItemIdx()
-function smcItemIdx(){
-  let smcCard = smcat.innerHTML;
-  var smcIndex = document.querySelectorAll('#smcSect .filmes-card').length; 
-  for(var n =0; n < 4; n++){
-    let item = brandData[ele].Category[smcIndex];
-    // console.log(item);
-    smcIndex++
-
-    switch (item.type) {
-      case 'youtube':
-        smcCard +=
-        '<div class="filmes-card">' +
-        '<div class="image-content">' +
-        '<img src="' +
-        item.filmTumb +
-        '" class="tumb" alt="">' +
-        '<div class="top-row">' +
-        '<div class="dimond">' +
-        '<img src="./img/king.png" alt=""></div>' +
-        '<div class="play-btn"><img src="./img/play-btn-small.png" class="youtube" ytSrc="' +item.filmBig +'"></div>' +
-        '</div>' +
-        '<div class="title">' +
-        item.catHashTag +
-        '</div>' +
-        '</div>' +
-        '</div>'
-        break
-      case 'htmlvid':
-      default:
-        smcCard +=
-        '<div class="filmes-card">' +
-        '<div class="image-content">' +
-        '<img src="' +
-        item.filmTumb +
-        '" class="tumb" alt="">' +
-        '<div class="top-row">' +
-        '<div class="dimond">' +
-        '<img src="./img/king.png" alt=""></div>' +
-        '<div class="play-btn"><img src="./img/play-btn-small.png" class="htmlvid" vidSrc="' + item.filmBig +'">' +
-        '</div>' +
-        '</div>' +
-        '<div class="title">' +
-        item.catHashTag +
-        '</div>' +
-        '</div>' +
-        '</div>'
-        break
-        
+      switch (item.type) {
+        case 'youtube':
+          smcCard +=
+            '<div class="filmes-card">' +
+            '<div class="image-content">' +
+            '<img src="' +
+            item.filmTumb +
+            '" class="tumb" alt="">' +
+            '<div class="top-row">' +
+            '<div class="dimond">' +
+            '<img src="./img/king.png" alt=""></div>' +
+            '<div class="play-btn"><img src="./img/play-btn-small.png" class="youtube" ytSrc="' +
+            item.filmBig +
+            '"></div>' +
+            '</div>' +
+            '<div class="title">' +
+            item.catHashTag +
+            '</div>' +
+            '</div>' +
+            '</div>'
+          break
+        case 'htmlvid':
+        default:
+          smcCard +=
+            '<div class="filmes-card">' +
+            '<div class="image-content">' +
+            '<img src="' +
+            item.filmTumb +
+            '" class="tumb" alt="">' +
+            '<div class="top-row">' +
+            '<div class="dimond">' +
+            '<img src="./img/king.png" alt=""></div>' +
+            '<div class="play-btn"><img src="./img/play-btn-small.png" class="htmlvid" vidSrc="' +
+            item.filmBig +
+            '">' +
+            '</div>' +
+            '</div>' +
+            '<div class="title">' +
+            item.catHashTag +
+            '</div>' +
+            '</div>' +
+            '</div>'
+          break
+      }
+      // if (smcIndex >= brandData[ele].Category.length) {
+      //   loadMbtn.style.display = 'none'
+      // }
     }
-    // if (smcIndex >= brandData[ele].Category.length) {
-    //   loadMbtn.style.display = 'none'
-    // }
-    
+    smcat.innerHTML = smcCard
   }
-  smcat.innerHTML = smcCard
-}
-
- 
 }
 // console.log()
 
@@ -172,7 +172,6 @@ function brdrpdwn(oplist) {
   var listData = brandData[oplist].brandTitle
   for (var i = 0; i > listData.length; i++) {}
 }
-
 
 let brDrpdwn = document.getElementById('brandName')
 
@@ -184,18 +183,55 @@ function brandDrpDwn(brandData) {
     optn.text = `${el.brandTitle}`
     // let option = <option value=' + '${el.brandTitle}' + >' + '${el.brandTitle}' + '</option>'
     brDrpdwn.append(optn)
-
   })
 }
 
+// use for UCATE
 
+const uCatSec = document.getElementById('uCategory')
+const ucatCount = document.querySelector('.ui-ux-section .second-title span')
+console.log(brandData[0].uCate.length)
+ucatCount.textContent = '(' + brandData[0].uCate.length + ')'
+
+function uCat(el) {
+  let uCat = ''
+  brandData[el].uCate.forEach((item, idx) => {
+    console.log('UCat =>', item.uCateMainTitle)
+    uCat +=
+      '<div class="item">' +
+      '<div class="art-card">' +
+      '<article class="info-web">' +
+      '<p class="inf-title">' +
+      item.uCateMainTitle +
+      '</p>' +
+      '<div class="sub-title">' +
+      item.uCateDesc +
+      '</div>' +
+      '<div class="di-flex sub-title link-box" >' +
+      '<a href="' +
+      item.uCateCaseStUrl +
+      '" class="red-text">Case study</a>| ' +
+      '<a href="' +
+      item.uCateWebUrl +
+      '">Visit Website</a>' +
+      '</div>' +
+      '</article>' +
+      '<div class="image-container">' +
+      '<img class="web-thumb" src="' +
+      item.uCateScreenshot +
+      '" alt="">' +
+      '</div>' +
+      '</div>' +
+      '</div>'
+    // const
+  })
+  uCatSec.innerHTML = uCat
+}
 
 // used for video popup
-(function () {
-
+;(function () {
   function setClickHandler(id, fn) {
     document.getElementById(id).onclick = fn
-
   }
   setClickHandler('filmCont', function (e) {
     var className = e.target.className
@@ -216,13 +252,13 @@ function brandDrpDwn(brandData) {
         dimensions: [1226, 900],
       })
     } else if (~className.indexOf('youtube')) {
-      console.log("click check");
+      console.log('click check')
       BigPicture({
         el: e.target,
         ytSrc: e.target.getAttribute('ytSrc'),
       })
     }
-  });
+  })
   setClickHandler('smcSect', function (e) {
     var className = e.target.className
     if (~className.indexOf('htmlvid')) {
@@ -231,41 +267,41 @@ function brandDrpDwn(brandData) {
         vidSrc: e.target.getAttribute('vidSrc'),
       })
     } else if (~className.indexOf('youtube')) {
-      console.log("click check");
+      console.log('click check')
       BigPicture({
         el: e.target,
         ytSrc: e.target.getAttribute('ytSrc'),
       })
     }
   })
-})();
+})()
 
 var Youtube = (function () {
-  'use strict';
+  'use strict'
 
-  var video, results;
+  var video, results
 
   var getThumb = function (url, size) {
-      if (url === null) {
-          return '';
-      }
-      size    = (size === null) ? 'big' : size;
-      results = url.match('[\\?&]v=([^&#]*)');
-      video   = (results === null) ? url : results[1];
+    if (url === null) {
+      return ''
+    }
+    size = size === null ? 'big' : size
+    results = url.match('[\\?&]v=([^&#]*)')
+    video = results === null ? url : results[1]
 
-      if (size === 'small') {
-          return 'http://img.youtube.com/vi/' + video + '/2.jpg';
-      }
-      return 'http://img.youtube.com/vi/' + video + '/0.jpg';
-  };
+    if (size === 'small') {
+      return 'http://img.youtube.com/vi/' + video + '/2.jpg'
+    }
+    return 'http://img.youtube.com/vi/' + video + '/0.jpg'
+  }
 
   return {
-      thumb: getThumb
-  };
-}());
+    thumb: getThumb,
+  }
+})()
 
 //Example of usage:
 
-var thumb = Youtube.thumb('http://www.youtube.com/watch?v=sjWmky2EAkU', 'big');
+var thumb = Youtube.thumb('http://www.youtube.com/watch?v=sjWmky2EAkU', 'big')
 
-console.log(thumb);
+console.log(thumb)
